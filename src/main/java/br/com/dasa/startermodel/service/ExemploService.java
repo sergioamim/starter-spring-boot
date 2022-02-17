20c7242a058708b8fa16e705ec778a8c07526429
Sergio <sergio.junior@meupag.com.br>
2022-02-17 10:50:21 -0300

    Added pom parent commons

--- src/main/java/br/com/dasa/startermodel/service/ExemploService.java
+++ src/main/java/br/com/dasa/startermodel/service/ExemploService.java
@@ -1,19 +1,18 @@
 package br.com.dasa.startermodel.service;
 
-import br.com.dasa.startermodel.entity.Exemplo;
-import br.com.dasa.startermodel.repository.ExemploRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.dasa.startermodel.entity.Exemplo;
 
 @Service
 public class ExemploService {
 
     public List<Exemplo> getAll() {
 
-        List<Exemplo> exemploList = new ArrayList<>();
+        List<Exemplo> exemploList = new ArrayList();
         exemploList.add(Exemplo.builder().firstName("Starter").lastName("Model 1").build());
         exemploList.add(Exemplo.builder().firstName("Starter").lastName("Model 2").build());
 
